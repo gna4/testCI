c5ca374a1cf81e5ec4bf7c54a3762888e66b2d20
evgeniy ivchenko <evgeniy.ivchenko@gmail.com>
2016-02-17 18:21:36 +0200

    fix bla bla

--- tests/controllers/CalculatorTest.php
+++ tests/controllers/CalculatorTest.php
@@ -81,7 +81,7 @@ class CalculatorTest extends PHPUnit_Framework_TestCase {
      */
     public function testAdd5() {
         $this->assertEquals(
-                4
+                3
                 , $this->object->add(1, 2)
         );
     }
